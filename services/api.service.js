82817f36c41939fea75450bc9aaceed7d9b3b9dd
Naz <nazzzarr@gmail.com>
2022-06-03 11:37:55 +0300

    print weather CLI

--- services/api.service.js
+++ services/api.service.js
@@ -1,33 +1,43 @@
+import axios from 'axios';
 import https from 'https';
 import { getKeyValue, TOKEN_DICTIONARY } from './storage.service.js';
 
-export const getWeather = async (city) => {
-  console.log('🚀 ~ city', city);
+export const getIcon = (icon) => {
+  switch (icon.slice(0, -1)) {
+    case '01':
+      return '☀️';
+    case '02':
+      return '🌤️';
+    case '03':
+      return '🌥️';
+    case '04':
+      return '☁️';
+    case '09':
+      return '🌧️';
+    case '10':
+      return '🌦️';
+    default:
+      return '💤';
+  }
+};
 
-  const token = await getKeyValue(TOKEN_DICTIONARY.token);
-  console.log('🧟 ~ token', token);
+export const getWeather = async () => {
+  const token =
+    process.env.TOKEN ?? (await getKeyValue(TOKEN_DICTIONARY.token));
+
+  const city = process.env.CITY ?? (await getKeyValue(TOKEN_DICTIONARY.city));
 
   if (!token) {
     throw new Error('Have problem with token');
   }
 
-  const url = new URL(`https://api.openweathermap.org/data/2.5/weather`);
+  if (!city) {
+    throw new Error('Big problem with city');
+  }
 
-  url.searchParams.append('q', city);
-  url.searchParams.append('appid', token);
-
-  https.get(url, (result) => {
-    let res = '';
-
-    result.on('data', (chunk) => {
-      res += chunk;
-    });
-    result.on('end', () => {
-      console.log('END');
-      console.log(res);
-    });
-    result.on('error', (error) => {
-      console.log(error.message);
-    });
-  });
+  const { data } = await axios.get(
+    `https://api.openweathermap.org/data/2.5/weather`,
+    { params: { q: city, appid: token } }
+  );
+  return data;
 };
--- weather.js
+++ weather.js
@@ -1,8 +1,13 @@
 #!/user/bin/env node
 
 import { getArgs } from './helpers/args.js';
-import { getWeather } from './services/api.service.js';
-import { printError, printHelp, printSuccess } from './services/log.service.js';
+import { getIcon, getWeather } from './services/api.service.js';
+import {
+  printError,
+  printHelp,
+  printSuccess,
+  printWeather,
+} from './services/log.service.js';
 import { saveKeyValue, TOKEN_DICTIONARY } from './services/storage.service.js';
 
 const saveToken = async (token) => {
@@ -19,20 +24,51 @@ const saveToken = async (token) => {
   }
 };
 
+const saveCity = async (city) => {
+  if (!city.length) {
+    printError('Dont have city, please add city -s [CITY_NAME]');
+    return;
+  }
+
+  try {
+    saveKeyValue(TOKEN_DICTIONARY.city, city);
+    printSuccess('City was saved');
+  } catch (error) {
+    printError(error.message);
+  }
+};
+
+const getForecast = async () => {
+  try {
+    const weather = await getWeather();
+    // console.log('object🙂', weather);
+    printWeather(weather, getIcon(weather.weather[0].icon));
+  } catch (error) {
+    if (error?.response?.status === 404) {
+      printError('Incorrect city');
+    } else if (error?.response?.status === 401) {
+      printError('Incorrect token');
+    } else {
+      printError(error.message);
+    }
+  }
+};
+
 const initCLI = () => {
   const args = getArgs(process.argv);
   if (args.h) {
-    printHelp();
+    return printHelp();
   }
 
   if (args.s) {
+    return saveCity(args.s);
   }
 
   if (args.t) {
     return saveToken(args.t);
   }
 
-  getWeather('Kharkiv');
+  return getForecast();
 };
 
 initCLI();
